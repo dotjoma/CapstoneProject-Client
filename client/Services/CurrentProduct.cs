5fb56598ff357decfc1d9f0dbfe8e4b3652ecd47
dotjoma <bastejoma8@gmail.com>
2025-02-22 10:11:56 +0800

    Upload CurrentProduct.cs

---
+++ client/Services/CurrentProduct.cs
@@ -0,0 +1,35 @@
+﻿using client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client.Services
+{
+    public class CurrentProduct
+    {
+        private static Product? _currentProduct;
+        private static List<Product> _allProduct = new List<Product>();
+
+        public static Product? Current => _currentProduct;
+        public static List<Product> AllProduct => _allProduct;
+
+        public static void SetProducts(List<Product> products)
+        {
+            _allProduct = products ?? new List<Product>();
+            _currentProduct = null;
+        }
+
+        public static void SetCurrentProduct(Product? product)
+        {
+            _currentProduct = product;
+        }
+
+        public static void Clear()
+        {
+            _currentProduct = null;
+            _allProduct.Clear();
+        }
+    }
+}
