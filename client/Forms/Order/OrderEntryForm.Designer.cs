99e490113a59eb015b45435a417b8a88246845cb
dotjoma <bastejoma8@gmail.com>
2025-02-23 01:17:45 +0800

    Update OrderEntryForm.Design.cs

--- client/Forms/Order/OrderEntryForm.Designer.cs
+++ client/Forms/Order/OrderEntryForm.Designer.cs
@@ -31,18 +31,6 @@
             components = new System.ComponentModel.Container();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            pnlContainer = new Panel();
-            btnBeverages = new Guna.UI2.WinForms.Guna2Button();
-            btnMainCourse = new Guna.UI2.WinForms.Guna2Button();
-            btnFastFood = new Guna.UI2.WinForms.Guna2Button();
             guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
             cartPanel = new FlowLayoutPanel();
             panel2 = new Panel();
@@ -50,79 +38,11 @@
             lblDateTime = new Label();
             label1 = new Label();
             panel1 = new Panel();
-            guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
             timer1 = new System.Windows.Forms.Timer(components);
+            panel3 = new Panel();
+            pnlContainer = new Panel();
             guna2Panel1.SuspendLayout();
-            guna2Panel2.SuspendLayout();
             SuspendLayout();
-            // 
-            // pnlContainer
-            // 
-            pnlContainer.Dock = DockStyle.Fill;
-            pnlContainer.Location = new Point(0, 119);
-            pnlContainer.Name = "pnlContainer";
-            pnlContainer.Size = new Size(709, 649);
-            pnlContainer.TabIndex = 21;
-            // 
-            // btnBeverages
-            // 
-            btnBeverages.BorderRadius = 5;
-            btnBeverages.CheckedState.ForeColor = Color.White;
-            btnBeverages.CustomizableEdges = customizableEdges1;
-            btnBeverages.DisabledState.BorderColor = Color.DarkGray;
-            btnBeverages.DisabledState.CustomBorderColor = Color.DarkGray;
-            btnBeverages.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
-            btnBeverages.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
-            btnBeverages.FillColor = Color.FromArgb(214, 192, 179);
-            btnBeverages.Font = new Font("Arial", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnBeverages.ForeColor = Color.FromArgb(98, 87, 87);
-            btnBeverages.Location = new Point(21, 49);
-            btnBeverages.Name = "btnBeverages";
-            btnBeverages.ShadowDecoration.CustomizableEdges = customizableEdges2;
-            btnBeverages.Size = new Size(110, 45);
-            btnBeverages.TabIndex = 8;
-            btnBeverages.Text = "Beverages";
-            btnBeverages.Click += btnBeverages_Click;
-            // 
-            // btnMainCourse
-            // 
-            btnMainCourse.BorderRadius = 5;
-            btnMainCourse.CheckedState.ForeColor = Color.White;
-            btnMainCourse.CustomizableEdges = customizableEdges3;
-            btnMainCourse.DisabledState.BorderColor = Color.DarkGray;
-            btnMainCourse.DisabledState.CustomBorderColor = Color.DarkGray;
-            btnMainCourse.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
-            btnMainCourse.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
-            btnMainCourse.FillColor = Color.FromArgb(214, 192, 179);
-            btnMainCourse.Font = new Font("Arial", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnMainCourse.ForeColor = Color.FromArgb(98, 87, 87);
-            btnMainCourse.Location = new Point(137, 49);
-            btnMainCourse.Name = "btnMainCourse";
-            btnMainCourse.ShadowDecoration.CustomizableEdges = customizableEdges4;
-            btnMainCourse.Size = new Size(110, 45);
-            btnMainCourse.TabIndex = 9;
-            btnMainCourse.Text = "Main Course";
-            btnMainCourse.Click += btnMainCourse_Click;
-            // 
-            // btnFastFood
-            // 
-            btnFastFood.BorderRadius = 5;
-            btnFastFood.CheckedState.ForeColor = Color.White;
-            btnFastFood.CustomizableEdges = customizableEdges5;
-            btnFastFood.DisabledState.BorderColor = Color.DarkGray;
-            btnFastFood.DisabledState.CustomBorderColor = Color.DarkGray;
-            btnFastFood.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
-            btnFastFood.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
-            btnFastFood.FillColor = Color.FromArgb(214, 192, 179);
-            btnFastFood.Font = new Font("Arial", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnFastFood.ForeColor = Color.FromArgb(98, 87, 87);
-            btnFastFood.Location = new Point(253, 49);
-            btnFastFood.Name = "btnFastFood";
-            btnFastFood.ShadowDecoration.CustomizableEdges = customizableEdges6;
-            btnFastFood.Size = new Size(110, 45);
-            btnFastFood.TabIndex = 10;
-            btnFastFood.Text = "Fast Food";
-            btnFastFood.Click += btnFastFood_Click;
             // 
             // guna2Panel1
             // 
@@ -135,12 +55,12 @@
             guna2Panel1.Controls.Add(panel1);
             guna2Panel1.CustomBorderColor = Color.LightGray;
             guna2Panel1.CustomBorderThickness = new Padding(1, 0, 0, 0);
-            guna2Panel1.CustomizableEdges = customizableEdges7;
+            guna2Panel1.CustomizableEdges = customizableEdges1;
             guna2Panel1.Dock = DockStyle.Right;
-            guna2Panel1.Location = new Point(709, 119);
+            guna2Panel1.Location = new Point(709, 0);
             guna2Panel1.Name = "guna2Panel1";
-            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges8;
-            guna2Panel1.Size = new Size(315, 649);
+            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            guna2Panel1.Size = new Size(315, 768);
             guna2Panel1.TabIndex = 19;
             // 
             // cartPanel
@@ -150,13 +70,13 @@
             cartPanel.Dock = DockStyle.Fill;
             cartPanel.Location = new Point(0, 119);
             cartPanel.Name = "cartPanel";
-            cartPanel.Size = new Size(315, 402);
+            cartPanel.Size = new Size(315, 521);
             cartPanel.TabIndex = 8;
             // 
             // panel2
             // 
             panel2.Dock = DockStyle.Bottom;
-            panel2.Location = new Point(0, 521);
+            panel2.Location = new Point(0, 640);
             panel2.Name = "panel2";
             panel2.Size = new Size(315, 128);
             panel2.TabIndex = 7;
@@ -198,24 +118,25 @@
             panel1.Size = new Size(315, 119);
             panel1.TabIndex = 6;
             // 
-            // guna2Panel2
-            // 
-            guna2Panel2.Controls.Add(btnBeverages);
-            guna2Panel2.Controls.Add(btnMainCourse);
-            guna2Panel2.Controls.Add(btnFastFood);
-            guna2Panel2.CustomBorderColor = Color.LightGray;
-            guna2Panel2.CustomBorderThickness = new Padding(0, 0, 0, 1);
-            guna2Panel2.CustomizableEdges = customizableEdges9;
-            guna2Panel2.Dock = DockStyle.Top;
-            guna2Panel2.Location = new Point(0, 0);
-            guna2Panel2.Name = "guna2Panel2";
-            guna2Panel2.ShadowDecoration.CustomizableEdges = customizableEdges10;
-            guna2Panel2.Size = new Size(1024, 119);
-            guna2Panel2.TabIndex = 20;
-            // 
             // timer1
             // 
             timer1.Tick += timer1_Tick;
+            // 
+            // panel3
+            // 
+            panel3.Dock = DockStyle.Top;
+            panel3.Location = new Point(0, 0);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(709, 119);
+            panel3.TabIndex = 22;
+            // 
+            // pnlContainer
+            // 
+            pnlContainer.Dock = DockStyle.Fill;
+            pnlContainer.Location = new Point(0, 119);
+            pnlContainer.Name = "pnlContainer";
+            pnlContainer.Size = new Size(709, 649);
+            pnlContainer.TabIndex = 23;
             // 
             // OrderEntryForm
             // 
@@ -223,26 +144,21 @@
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1024, 768);
             Controls.Add(pnlContainer);
+            Controls.Add(panel3);
             Controls.Add(guna2Panel1);
-            Controls.Add(guna2Panel2);
             Font = new Font("Verdana", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
             FormBorderStyle = FormBorderStyle.None;
             Name = "OrderEntryForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "OrderEntryForm";
+            FormClosed += OrderEntryForm_FormClosed;
             Load += OrderEntryForm_Load;
             guna2Panel1.ResumeLayout(false);
             guna2Panel1.PerformLayout();
-            guna2Panel2.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
-
-        private Panel pnlContainer;
-        private Guna.UI2.WinForms.Guna2Button btnBeverages;
-        private Guna.UI2.WinForms.Guna2Button btnMainCourse;
-        private Guna.UI2.WinForms.Guna2Button btnFastFood;
         private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
         private FlowLayoutPanel cartPanel;
         private Panel panel2;
@@ -250,7 +166,8 @@
         private Label lblDateTime;
         private Label label1;
         private Panel panel1;
-        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
         private System.Windows.Forms.Timer timer1;
+        private Panel panel3;
+        private Panel pnlContainer;
     }
 }
